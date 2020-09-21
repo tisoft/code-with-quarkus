b8cdb762edc056675ecf70d9d8bc9622ea939844
Markus Heberling <markus.heberling@advanova.de>
2020-09-21 16:11:34 +0200

    show bug

--- src/test/java/org/acme/ExampleResourceTest.java
+++ src/test/java/org/acme/ExampleResourceTest.java
@@ -1,6 +1,11 @@
 package org.acme;
 
+import java.net.URL;
+
+import io.quarkus.test.common.http.TestHTTPResource;
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -9,8 +14,12 @@ import static org.hamcrest.CoreMatchers.is;
 @QuarkusTest
 public class ExampleResourceTest {
 
+    @TestHTTPResource("/")
+    URL url;
+
     @Test
     public void testHelloEndpoint() {
+        Assertions.assertEquals(RestAssured.port, url.getPort());
         given()
           .when().get("/hello")
           .then()
